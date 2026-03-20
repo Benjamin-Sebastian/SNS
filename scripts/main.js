e25e60b7fb4494b8c291088da9a67dbcdc19305d
Benjamin-Sebastian <benjamin.sebastian@gmail.com>
2026-03-20 18:49:36 -0500

    dropdown spacing 7

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
-// document.querySelectorAll('.main-nav li').forEach(li => {
-//   const subnav = li.querySelector('.subnav');
-//   if (!subnav) return;
+function setHeaderHeight() {
+  document.documentElement.style.setProperty(
+    '--header-height',
+    `${header.offsetHeight}px`
+  );
+}
 
-//   const trigger = li.querySelector('a');
-
-//   trigger.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     li.classList.toggle('open');
-//   });
-// });
+setHeaderHeight();
+window.addEventListener('resize', setHeaderHeight);
