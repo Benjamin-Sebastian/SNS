5e0832a72b88cef8b9e2c9958f98c26880eed9b9
Benjamin-Sebastian <benjamin.sebastian@gmail.com>
2026-03-20 17:50:59 -0500

    Test Bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-document.querySelectorAll('.main-nav li').forEach(li => {
-  const subnav = li.querySelector('.subnav');
-  if (!subnav) return;
+// document.querySelectorAll('.main-nav li').forEach(li => {
+//   const subnav = li.querySelector('.subnav');
+//   if (!subnav) return;
 
-  const trigger = li.querySelector('a');
+//   const trigger = li.querySelector('a');
 
-  trigger.addEventListener('click', (e) => {
-    e.preventDefault();
-    li.classList.toggle('open');
-  });
-});
+//   trigger.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     li.classList.toggle('open');
+//   });
+// });
