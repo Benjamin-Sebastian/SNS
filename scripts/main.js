1a0a6a4b8cb7a34d203523c90df66d688adb5a84
Benjamin-Sebastian <benjamin.sebastian@gmail.com>
2026-03-20 17:04:39 -0500

    added click toggle to folder

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.main-nav li').forEach(li => {
+  const subnav = li.querySelector('.subnav');
+  if (!subnav) return;
+
+  const trigger = li.querySelector('a');
+
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+    li.classList.toggle('open');
+  });
+});
