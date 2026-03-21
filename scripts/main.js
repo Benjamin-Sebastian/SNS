123c6c0d6781053196a0eeafa5ce58508eb066fc
Benjamin-Sebastian <benjamin.sebastian@gmail.com>
2026-03-20 20:05:10 -0500

    Dropdown arrow change 8

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,29 @@
 document.addEventListener('shown.bs.dropdown', function (e) {
     const toggle = e.relatedTarget;
-    const menu   = toggle.closest('.dropdown').querySelector('.dropdown-menu');
+    const menu = toggle.closest('.dropdown').querySelector('.dropdown-menu');
     const header = document.getElementById('header');
 
     const headerRect = header.getBoundingClientRect();
     const toggleRect = toggle.getBoundingClientRect();
-    const navItem    = toggle.closest('.nav-item');
-    const isLast     = !navItem.nextElementSibling;
+    const navItem = toggle.closest('.nav-item');
+    const isLast = !navItem.nextElementSibling;
 
     // position: fixed escapes all CSS contexts; coordinates are viewport-relative
-    menu.style.position  = 'fixed';
-    menu.style.top       = headerRect.bottom + 'px';
+    menu.style.position = 'fixed';
+    menu.style.top = headerRect.bottom + 'px';
     menu.style.transform = 'none';
 
     if (isLast) {
-        menu.style.left  = 'auto';
+        menu.style.left = 'auto';
         menu.style.right = (window.innerWidth - toggleRect.right) + 'px';
     } else {
-        menu.style.left  = toggleRect.left + 'px';
+        menu.style.left = toggleRect.left + 'px';
         menu.style.right = 'auto';
     }
-    const arrows = document.getElementsByClassName("arrow");
-    arrows.forEach(arrow => {
-        arrow.innerText = "&uarr;"
-    });
+    var arrows = document.getElementsByClassName("arrow");
+    for (var i = 0; i < arrows.length; i++) {
+        arrows[i].innerText = "&uarr;";
+    }
 });
 
 document.addEventListener('hidden.bs.dropdown', function (e) {
@@ -33,13 +33,13 @@ document.addEventListener('hidden.bs.dropdown', function (e) {
     if (!menu) return;
 
     // Reset so Popper starts clean on next open
-    menu.style.position  = '';
-    menu.style.top       = '';
-    menu.style.left      = '';
-    menu.style.right     = '';
+    menu.style.position = '';
+    menu.style.top = '';
+    menu.style.left = '';
+    menu.style.right = '';
     menu.style.transform = '';
-    const arrows = document.getElementsByClassName("arrow");
-    arrows.forEach(arrow => {
-        arrow.innerText = "&darr;"
-    });
+    var arrows = document.getElementsByClassName("arrow");
+    for (var i = 0; i < arrows.length; i++) {
+        arrows[i].innerText = "&darr;";
+    }
 });
